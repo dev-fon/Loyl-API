03f5e97eef2c3b51ce719ec72cc75a43a45a0537
Denis Fonjga <dfonjga@yahoo.com>
2019-10-20 16:55:09 -0700

    Init Loyl API

---
+++ Controllers/UserDto.cs
@@ -0,0 +1,10 @@
+namespace loyl.Controllers
+{
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
+    }
+}
